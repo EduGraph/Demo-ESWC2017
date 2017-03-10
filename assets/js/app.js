2bb627a46e91b5499758c90298bfa3f43237a48b
Jonas Jetschni <jetschni@gmail.com>
2017-03-10 17:07:55 +0100

    Added card section technologies

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,4 +1,23 @@
 $( document ).ready(function() {
 
     $('#architecture-svg').load('images/edugraph-architecture.svg');
+
+/*
+    $( '.screenshot-gallery' ).click( function( e ) {
+        e.preventDefault();
+        $.swipebox(
+        [
+            { href:'images/StudyFinder_ScrSh1.png', title:'My Caption' },
+            { href:'images/StudyFinder_ScrSh2.png', title:'My Second Caption' },
+            { href:'images/StudyFinder_ScrSh3.png', title:'My Second Caption' },
+            { href:'images/StudyFinder_ScrSh4.png', title:'My Second Caption' }
+        ],
+        {
+            hideCloseButtonOnMobile : false, // true will hide the close button on mobile devices
+            removeBarsOnMobile : false, // false will show top bar on mobile devices
+            hideBarsDelay : 0, // delay before hiding bars on desktop
+            loopAtEnd: true // true will return to the first image after the last image is reached
+        });
+    } );
+    */
 });
--- assets/js/navigation.js
+++ assets/js/navigation.js
@@ -19,17 +19,48 @@ $( document ).ready(function() {
         $("#card-detail-text").text(mappingDetails['description']);
         $("#card-detail-badge").text(mappingDetails['usecase']);
         $("#card-detail-image").attr("src", mappingDetails['image']);
+
+
         $("#card-detail-links").empty();
-        mappingDetails['links'].forEach(function(e,i){
-            $("#card-detail-links").append('<a href="'+e['url']+'" target="_blank" class="card-link">'+e['label']+'</a>');
-        });
+        if(mappingDetails.hasOwnProperty('links')){
+            mappingDetails['links'].forEach(function(e,i){
+                $("#card-detail-links").append('<a href="'+e['url']+'" target="_blank" class="card-link">'+e['label']+'</a>');
+            });
+        }
 
-        $("#card-detail-technology").empty();
 
-        /*mappingDetails['technologies'].forEach(function(e,i){
-            $("#section-technology").append('<a href="'+e['url']+'" target="_blank" class="card-link">'+e['label']+'</a>');
-        });*/
+
+        if(mappingDetails.hasOwnProperty('screenshots')){
+            var screenshots = [];
+
+            $("#card-detail-links").append('<a href="#" class="card-link screenshot-gallery">Screenshots</a>');
+
+            mappingDetails['screenshots'].forEach(function(e,i){
+                screenshots.push( { href:e.src, title: e.title })
+            });
+
+            $( '.screenshot-gallery' ).click( function( e ) {
+                e.preventDefault();
+                $.swipebox(
+                    screenshots,
+                    {
+                        hideCloseButtonOnMobile : false, // true will hide the close button on mobile devices
+                        removeBarsOnMobile : false, // false will show top bar on mobile devices
+                        hideBarsDelay : 0, // delay before hiding bars on desktop
+                        loopAtEnd: true // true will return to the first image after the last image is reached
+                    });
+            } );
+
+            console.log(screenshots);
+
+        }
+
+        $("#card-detail-technologies").empty();
+        if(mappingDetails.hasOwnProperty('technologies')){
+            mappingDetails['technologies'].forEach(function(e,i){
+                $("#section-technology").append('<a href="'+e['url']+'" target="_blank" class="card-link">'+e['label']+'</a>');
+            });
+        }
     }
-
 });
 
